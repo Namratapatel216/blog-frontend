a7827335be127752de945720eec9448d73ac0871
Your Name <you@example.com>
2019-08-07 15:38:16 +0530

    my-first-commit-css

---
+++ 1.js
@@ -0,0 +1,12 @@
+// armstrong number.
+function check_armstrong(inputed_no)
+{
+    alert(inputed_no);
+ 
+}
+
+var no1 = prompt("enter the no to check whether it is armstrong or not");
+
+var result = check_armstrong(no1);
+
+alert(result);
---
+++ test.js
@@ -0,0 +1,18 @@
+let a="firstName"
+
+let b="lastName"
+
+let test_data={
+
+a:"Subhanshu",
+
+b:"Bigasia"
+
+}
+
+for(let i in test_data){
+
+alert(i);
+alert(test_data[i]);
+
+}
